37c0d80ad5ce851489418501fc0f4f2e33bdd915
Gustavo Neves <gagneves@hotmail.com>
2023-03-17 11:45:49 -0400

    Red Color

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import './header.css'
 import { Link } from 'react-router-dom';
 
-function Header(){
-    return(
+function Header() {
+    return (
         <header>
-            <Link className='logo' to='/'>Nevinho Flix</Link>
+            <Link className='logo' to='/'>NevinhoFlix - Top1</Link>
             <Link className='favoritos' to='/favoritos'>Meus Filmes</Link>
-            
+
         </header>
     )
 }
